155fca9726a568fa73ae2bdd5386271e554ae3bd
heff <ksuder7@gmail.com>
2019-11-13 18:59:36 +0100

    create paddles and ball

--- game.py
+++ game.py
@@ -22,4 +22,27 @@ playing_field.goto(-boarder_x, -boarder_y)
 playing_field.goto(-boarder_x, boarder_y)
 playing_field.goto(boarder_x, boarder_y)
 
+# Left Paddle
+left_paddle = turtle.Turtle()
+left_paddle.speed(0)
+left_paddle.shape("square")
+left_paddle.shapesize(stretch_wid=5, stretch_len=1)
+left_paddle.penup()
+left_paddle.goto(-boarder_x + 15, 0)
+
+# Right Paddle
+right_paddle = turtle.Turtle()
+right_paddle.speed(0)
+right_paddle.shape("square")
+right_paddle.shapesize(stretch_wid=5, stretch_len=1)
+right_paddle.penup()
+right_paddle.goto(boarder_x + -15, 0)
+
+# Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.penup()
+ball.goto(0, 0)
+
 window.mainloop()
