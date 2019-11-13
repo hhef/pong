ff915078308b564ca5fef3c064a7844e8a1cdeaf
heff <ksuder7@gmail.com>
2019-11-13 18:47:10 +0100

    create playing boarder

--- game.py
+++ game.py
@@ -5,7 +5,21 @@ window = turtle.Screen()
 window.title("Pong")
 window.bgcolor("orange")
 window.setup(width=1000, height=800)
-window.tracer(0)
 
-while True:
-    window.update()
+# Playing field
+boarder_x = 420
+boarder_y = 250
+
+playing_field = turtle.Turtle()
+playing_field.hideturtle()
+playing_field.color("black")
+playing_field.speed(5)
+playing_field.penup()
+playing_field.goto(boarder_x, boarder_y)
+playing_field.pendown()
+playing_field.goto(boarder_x, -boarder_y)
+playing_field.goto(-boarder_x, -boarder_y)
+playing_field.goto(-boarder_x, boarder_y)
+playing_field.goto(boarder_x, boarder_y)
+
+window.mainloop()
